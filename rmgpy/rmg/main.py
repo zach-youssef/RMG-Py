eb3bd6763a20a32d22f09d8ac4b32f898309ce15
Matt Johnson <mjohnson541@gmail.com>
2017-04-18 10:35:02 -0400

    setting a max number of species termination criterion
    Enables setting of a max number of species at which the model will complete simulation of that settings iteration and move to the next iteration
    When the run terminates this way it effectively continues the settings for loop in main

--- rmgpy/rmg/main.py
+++ rmgpy/rmg/main.py
@@ -578,6 +578,8 @@ class RMG(util.Subject):
         assert len(self.modelSettingsList) == len(self.simulatorSettingsList), "different number of model and simulator declarations in input file with more than one simulator declaration"
         
         self.makeSeedMech(firstTime=True)
+
+        maxNumSpcsHit = False #default
         
         for q,modelSettings in enumerate(self.modelSettingsList):
             if len(self.simulatorSettingsList) > 1: 
@@ -662,7 +664,13 @@ class RMG(util.Subject):
                     
                     # Add objects to enlarge to the core first
                     for objectToEnlarge in objectsToEnlarge:
+                        if len(self.reactionModel.core.species) >= modelSettings.maxNumSpecies:
+                            maxNumSpcsHit = True
+                            break
                         self.reactionModel.enlarge(objectToEnlarge)
+                        
+                    if maxNumSpcsHit: #breaks the while loop 
+                        break
                     
                     if len(self.reactionModel.core.species) > numCoreSpecies:
                         tempModelSettings = deepcopy(modelSettings)
@@ -713,7 +721,10 @@ class RMG(util.Subject):
                         logging.info('The current model core has %s species and %s reactions' % (coreSpec, coreReac))
                         logging.info('The current model edge has %s species and %s reactions' % (edgeSpec, edgeReac))
                         return
-        
+                    
+            if maxNumSpcsHit: #resets maxNumSpcsHit and continues the settings for loop
+                maxNumSpcsHit = False
+                continue
         
         # Run sensitivity analysis post-model generation if sensitivity analysis is on
         for index, reactionSystem in enumerate(self.reactionSystems):
